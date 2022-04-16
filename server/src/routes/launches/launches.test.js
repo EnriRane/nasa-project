8be7744f415fd852b9cd447662e9062e3cf49a72
Enri Rane <enrirane@gmail.com>
2022-04-16 13:29:30 +0200

    Load planets in test

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,71 @@
+const request = require('supertest');
+const app = require('../../app');
+const { loadPlanetsData } = require('../../models/planets.model');
+const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
+
+describe('Launches API', () => {
+  beforeAll(async () => {
+    await mongoConnect();
+    await loadPlanetsData();
+  });
+  afterAll(async () => {
+    await mongoDisconnect();
+  });
+  describe('Test GET /launches', () => {
+    test('it should response with 200 success', async () => {
+      await request(app).get('/v1/launches').expect(200);
+    });
+  });
+
+  describe('Test POST /launches', () => {
+    const completeLaunchData = {
+      mission: 'USS Enterprise',
+      rocket: 'NCC 1701-D',
+      target: 'Kepler-62 f',
+      launchDate: 'January 4,2028',
+    };
+    const launchDatawithNoDate = {
+      mission: 'USS Enterprise',
+      rocket: 'NCC 1701-D',
+      target: 'Kepler-62 f',
+    };
+    const launchDatawithInvalidDate = {
+      mission: 'USS Enterprise',
+      rocket: 'NCC 1701-D',
+      target: 'Kepler-62 f',
+      launchDate: 'zoot',
+    };
+    test('it should response with 201 success', async () => {
+      const response = await request(app)
+        .post('/v1/launches')
+        .send(completeLaunchData)
+        .expect(201);
+
+      const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+      const responseDate = new Date(response.body.launchDate).valueOf();
+
+      expect(responseDate).toBe(requestDate);
+      expect(response.body).toMatchObject(launchDatawithNoDate);
+    });
+    test('it should catch missing required properties', async () => {
+      const response = await request(app)
+        .post('/v1/launches')
+        .send(launchDatawithNoDate)
+        .expect(400);
+
+      expect(response.body).toStrictEqual({
+        error: 'Missing required launch property',
+      });
+    });
+    test('it should catch invalid dates', async () => {
+      const response = await request(app)
+        .post('/v1/launches')
+        .send(launchDatawithInvalidDate)
+        .expect(400);
+
+      expect(response.body).toStrictEqual({
+        error: 'Invalid launch Date',
+      });
+    });
+  });
+});
